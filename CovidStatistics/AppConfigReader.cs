7a6bd20406eea50029ab1703759b3bdaf1e5ad9a
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-03-17 14:09:10 +0000

    implemented scenario for when not logged in not able to see the day record test

--- CovidStatistics/AppConfigReader.cs
+++ CovidStatistics/AppConfigReader.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CovidStatisticsTest
 {
-    class AppConfigReader
+    public class AppConfigReader
     {
+        // needs to be inside BDD folder
+        //public static readonly string HomePageUrl = ConfigurationManager.AppSettings["homepage_url"];
+
+        public static readonly string HomePageUrl = "https://localhost:44320/";
     }
 }
--- CovidStatistics/lib/pages/CS_HomePage.cs
+++ CovidStatistics/lib/pages/CS_HomePage.cs
@@ -1,8 +1,10 @@
-﻿using OpenQA.Selenium;
+﻿using CovidStatisticsTest;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CovidStatistics.lib.pages
@@ -15,5 +17,34 @@ namespace CovidStatistics.lib.pages
         }
 
         public IWebDriver Driver { get; }
+
+        // url of page
+        private readonly string _homePageUrl = AppConfigReader.HomePageUrl;
+        //private readonly string _test = "https://google.com";
+
+        // elements
+
+
+        // helper methods for test
+        public void Visit()
+        {
+            Driver.Navigate().GoToUrl(_homePageUrl);
+            Thread.Sleep(3000);
+        }
+
+        public bool DoesDayRecordLinkExist()
+        {
+            try
+            {
+                Driver.FindElement(By.LinkText("Day Records"));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
     }
 }
