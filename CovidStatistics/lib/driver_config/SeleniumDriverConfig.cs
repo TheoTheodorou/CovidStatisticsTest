c8343f0c8acaf19829d8b8cf604523c274c8bab9
Theo Theodorou <theodore.theodorou@mac.com>
2021-03-17 11:07:26 +0000

    Initial Commit

---
+++ CovidStatistics/lib/driver_config/SeleniumDriverConfig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumWalkthrough.lib.driver_config
+{
+    public class SeleniumDriverConfig
+    {
+        public IWebDriver Driver { get; set; }
+
+        public SeleniumDriverConfig(string driver, int PageLoadInSecs, int implicitWaitInSecs)
+        {
+            DriverSetUp(driver, PageLoadInSecs, implicitWaitInSecs);
+        }
+
+        private void DriverSetUp(string driver, int pageLoadInSecs, int implicitWaitInSecs)
+        {
+            switch (driver)
+            {
+                case "chrome":
+                    SetChromeDriver();
+                    SetDriverConfiguration(pageLoadInSecs, implicitWaitInSecs);
+                    break;
+                case "firefox":
+                    SetFirefoxDriver();
+                    SetDriverConfiguration(pageLoadInSecs, implicitWaitInSecs);
+                    break;
+                default:
+                    throw new Exception("Please use chrome or firefox");
+            }
+        }
+
+        private void SetFirefoxDriver()
+        {
+            Driver = new FirefoxDriver();
+        }
+
+        private void SetChromeDriver()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        private void SetDriverConfiguration(int pageLoadInSecs, int implicitWaitInSecs)
+        {
+            Driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(pageLoadInSecs);
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(implicitWaitInSecs);
+        }
+    }
+}
