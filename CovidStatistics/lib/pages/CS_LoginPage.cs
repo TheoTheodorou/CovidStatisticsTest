89b7f5172064f99f71ceaf01e931a733c31f1a35
jkchuong <jkchuong@gmail.com>
2021-03-17 11:17:25 +0000

    Fixed theo mistakes

--- CovidStatistics/lib/pages/CS_LoginPage.cs
+++ CovidStatistics/lib/pages/CS_LoginPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace CovidStatistics.lib.pages
 {
-    class CS_LoginPage
+    public class CS_LoginPage
     {
+        public CS_LoginPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebDriver Driver { get; }
     }
 }
